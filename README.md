6772da45481cde097e1f4ddd200b4c8ac6dc8507
Balaji Sivaraman <balaji@balajisivaraman.com>
2018-03-22 20:41:12 +0530

    Initial commit, with some slides copied over from Rust in Peace

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rust for Rubyists
+
+This the Latex slide source for the Rust for Rubyists talk given by me
+for the Chennai Ruby Meetup held in ThoughtWorks Chennai office on
+March 24, 2018.
---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+SHOULD_GENERATE_BIB=${1:-0}
+SHOULD_CLEAN=${2:-0}
+
+OUTPUT_DIR=output
+XELATEX_CMD="xelatex -output-directory=$OUTPUT_DIR --shell-escape rust-for-rubyists.tex"
+BIBER_CMD="biber output/rust-for-rubyists"
+
+function clean() {
+    rm -rf $OUTPUT_DIR
+    mkdir $OUTPUT_DIR
+}
+
+function build() {
+    $XELATEX_CMD
+    if [ "$SHOULD_GENERATE_BIB" -eq "1" ]; then
+        $BIBER_CMD
+        $XELATEX_CMD
+    fi
+}
+
+function main() {
+    if [ "$SHOULD_CLEAN" -eq "1" ]; then
+        clean
+    fi
+    build
+}
+
+main
